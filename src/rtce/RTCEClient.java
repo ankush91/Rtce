23029aef1d1f2743f565176a8c342ef92fc1a7d3
Anthony <ape22@drexel.edu>
2016-05-14 21:59:09 -0400

    Added in server discovery.   Defaults to 127.0.0.1 when not discovered

--- src/rtce/RTCEClient.java
+++ src/rtce/RTCEClient.java
@@ -16,10 +16,15 @@ public class RTCEClient {
         OutputStream sendStream;
         InputStream recvStream;
         String request, response;
+
+        //Constants used for Discovery
+    	final static int    DISCOVERY_PORT = 4446;
+    	final static String MCAST_DISCOVERY_GROUP = "225.0.0.10";	
         
-        RTCEClient(String server, int port) throws IOException, UnknownHostException
+        RTCEClient(int port) throws IOException, UnknownHostException
                 {
-                    sock = new Socket (server, port);
+        	        
+                    sock = new Socket (discoverServer(), port);
                     sendStream = sock.getOutputStream();
                     recvStream = sock.getInputStream();
                 }
@@ -84,13 +89,80 @@ public class RTCEClient {
         }       
         
         
+        /* discoverServer will attempt to locate the IP address of any RTCE server
+         *     if none could be found the loopback adapter 127.0.0.1 will be returned
+         */
+        String discoverServer()
+        {
+        	byte[] outboundBuf = new byte[4];
+        	
+        	try{
+            //Create the socket and join the Multicast Group for Discovery
+        	MulticastSocket socket = new MulticastSocket(DISCOVERY_PORT);
+            InetAddress m_group = InetAddress.getByName(MCAST_DISCOVERY_GROUP);
+            socket.joinGroup(m_group);
+        	
+            //Create the Datagram with this client's IP Address as the payload
+            DatagramPacket outboundPacket;
+            System.out.println("Client " + InetAddress.getLocalHost().getHostAddress() + 
+            		" is searching for a server...");
+            outboundBuf = InetAddress.getLocalHost().getAddress();
+            outboundPacket = new DatagramPacket(
+            		outboundBuf,
+            		outboundBuf.length,
+            		m_group,DISCOVERY_PORT);
+            
+            //Prepare to send packet and receive response from server
+            int attempts = 0;
+            socket.setSoTimeout(1000);     //Give the server 1 second to respond
+            socket.setLoopbackMode(true);  //Don't want to hear our own outbound msgs
+            byte[] responseBuf = new byte[2];
+            DatagramPacket responsePacket;
+            responsePacket = new DatagramPacket(
+            		responseBuf,
+            		responseBuf.length,
+            		m_group,DISCOVERY_PORT);
+            
+            //Make 5 attempts to listen for a server
+            while (attempts < 5)
+            {  socket.send(outboundPacket);
+               try {
+                 socket.receive(responsePacket);
+                 if (responseBuf[0] == 'H' & responseBuf[1] == 'I')
+                 { System.out.println("Found Server: " + responsePacket.getAddress());
+                   socket.close();
+                   return responsePacket.getAddress().getHostAddress();
+                 }
+                 else
+                 { System.out.println("Got a response, but not from a RTCE Server");
+                   attempts++;
+                 }
+               } catch (java.net.SocketTimeoutException e)
+               { attempts++; /*No response try again.*/
+                 System.out.println("No server response: Attempt " + attempts + " of 5");
+               }                   
+            }
+            
+            //No server was found
+            System.out.println("No servers found.   Defaulting to 127.0.0.1");
+            socket.close();
+            return "127.0.0.1";
+        	} //try block
+            catch (IOException e) {
+            	e.printStackTrace(); }
+        	
+        	return "127.0.0.1";
+        } // end discoverServer()
+        
+        
     public static void main(String[] args) throws IOException
     {
-        final int servPort = 25351; //Server Port
-        final String servName = "127.0.0.1"; //Server Name
+        final int servPort = 25351; //Server Port        
         int i =0;
         
-        RTCEClient client = new RTCEClient(servName, servPort);
+        
+        RTCEClient client = new RTCEClient(servPort);
+        
         
         while(i<10)
         {
