570498328c4451c367643a80411ecbd6b8b7c57f
EdgeDauDrex <egd34@drexel.edu>
2016-05-16 16:26:42 -0400

    Added minimal authentication.

--- src/rtce/RTCEAuth.java
+++ src/rtce/RTCEAuth.java
@@ -3,7 +3,9 @@ package rtce;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 public class RTCEAuth {
 
@@ -22,10 +24,25 @@ public class RTCEAuth {
 	}
 	
 	
-	public boolean performAuth() throws FileNotFoundException{
+	public boolean performAuth() throws IOException{
 		if(validAuth()){
 			BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(RTCEServerConfig.getAuthFile()), RTCEServerConfig.getEncoding()));
-			
+			String line = reader.readLine();
+			while(line != null){
+				if(line.trim().equals("Username: " + message.getUsername())){
+					line = reader.readLine();
+					while(line != null && !line.trim().equals("")){
+						if(line.trim().equals("Password: " + message.getPassword())){
+							return true;
+						}else if(line.trim().startsWith("Password: ")){
+							return false;
+						}else{
+							line = reader.readLine();
+						}
+					}
+				}
+			}
+			return false;
 		}else{
 			return false;
 		}
--- src/rtce/RTCEMessage.java
+++ src/rtce/RTCEMessage.java
@@ -3,9 +3,19 @@ package rtce;
 public class RTCEMessage {
 
 	private RTCEMessageType request;
+	private String username;
+	private String password;
 
 	public RTCEMessageType getRequest() {
 		return request;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
 	}
 	
 	
