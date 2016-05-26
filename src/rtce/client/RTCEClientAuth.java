f923de8ab16787e3ab7f1adbb6dcf0e8c8b001df
EdgeDauDrex <egd34@drexel.edu>
2016-05-26 15:45:40 -0400

    Some client handshake initiation code

--- src/rtce/client/RTCEClientAuth.java
+++ src/rtce/client/RTCEClientAuth.java
@@ -1,5 +1,8 @@
 package rtce.client;
 
+import java.util.List;
+
+import rtce.RTCEMessageType;
 import rtce.server.RTCEServerMessage;
 
 public class RTCEClientAuth {
@@ -12,11 +15,74 @@ public class RTCEClientAuth {
 	private RTCEClientMessage clientMessage;
 	private RTCEServerMessage serverMessage;
 	
+	/**
+	 * Create the authentication module from user input
+	 * @param uname - the username
+	 * @param pword - the password
+	 * @param downer - the document owner
+	 * @param dtitle - the document title
+	 */
 	public RTCEClientAuth(String uname, String pword, String downer, String dtitle){
 		username = uname;
 		password = pword;
 		documentOwner = downer;
 		documentTitle = dtitle;
+		buildMessage();
+	}
+
+	/**
+	 * Build the client authentication message
+	 */
+	private void buildMessage(){
+		clientMessage = new ControlMessage();
+		clientMessage.setRequest(RTCEMessageType.CUAUTH);
+		clientMessage.setDocumentOwner(documentOwner);
+		clientMessage.setDocumentTitle(documentTitle);
+		clientMessage.setPassword(password);
+		clientMessage.setUsername(username);
+		clientMessage.setEncryptOpts(getEncryptOpts());
+		clientMessage.setGenericOpts(getGenericOpts());
 	}
 	
+	/**
+	 * Get the client authentication message
+	 * @return the client authentication message
+	 */
+	public RTCEClientMessage getClientMessage() {
+		return clientMessage;
+	}
+
+	/**
+	 * Get the server connection response
+	 * @return the server connection response
+	 */
+	public RTCEServerMessage getServerMessage() {
+		return serverMessage;
+	}
+	
+	/**
+	 * Set the server connection response
+	 * @param serverMessage = the server connection response
+	 */
+	public void setServerMessage(RTCEServerMessage serverMessage) {
+		this.serverMessage = serverMessage;
+	}
+
+	/**
+	 * Get the ordered array of desired encryption options
+	 * @return an ordered string array
+	 */
+	private String[] getEncryptOpts(){
+		List<String> encrypt = RTCEClientConfig.getDesiredEncrypts();
+		return encrypt.toArray(new String[encrypt.size()]);
+	}
+	
+	/**
+	 * Get the array of desired generic options
+	 * @return a string array
+	 */
+	private String[] getGenericOpts(){
+		List<String> opts = RTCEClientConfig.getDesiredOpts();
+		return opts.toArray(new String[opts.size()]);
+	}
 }
